9bf19e25fc5a9a93fddb6bba18637ce74c31b348
sobadon <sbdn@chasoba.net>
2021-01-24 19:50:43 +0000

    不要なファイルを削除

--- generate.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-csv_list=`find csv -type f`
-format_list="skk contacts"
-for format in ${format_list} ; do for csv in ${csv_list} ; do ./syosyo --format ${format} `basename ${csv} .csv` ; done ; done
